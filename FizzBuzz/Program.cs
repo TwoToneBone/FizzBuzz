6be4d00b36ddc8b538b3b3b2af222094aa24b190
Erik <erik.ra@gmail.com>
2017-04-11 22:36:33 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            uint x = uint.Parse(inputs[0]);
+            uint y = uint.Parse(inputs[1]);
+            uint n = uint.Parse(inputs[2]);
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % x == 0 && i % y == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % x == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % y == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
